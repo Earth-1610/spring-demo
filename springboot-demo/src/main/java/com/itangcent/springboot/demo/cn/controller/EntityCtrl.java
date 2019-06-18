a73a78ca866374863c926556c827ca1f94f1e537
tangcent <sweettom@foxmail.com>
2019-06-18 11:30:22 +0800

    提供Spring Entity相关测试接口

---
+++ springboot-demo/src/main/java/com/itangcent/springboot/demo/cn/controller/EntityCtrl.java
@@ -0,0 +1,53 @@
+package com.itangcent.springboot.demo.cn.controller;
+
+
+import com.itangcent.springboot.demo.cn.dto.Result;
+import com.itangcent.springboot.demo.cn.model.UserInfo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Entity相关
+ */
+@RestController
+@RequestMapping(value = "/test/entity")
+public class EntityCtrl {
+
+    /**
+     * 测试ResponseEntity简单对象
+     */
+    @RequestMapping("/int")
+    public ResponseEntity<Integer> intInEntity(
+            @RequestHeader("token") String token,
+            @RequestBody RequestEntity<Integer> requestEntity) {
+        return ResponseEntity.ok(65536);
+    }
+
+    /**
+     * 测试ResponseEntity复合对象
+     */
+    @RequestMapping("/double")
+    public ResponseEntity<Result<Double>> doubleInEntity(
+            @RequestHeader("token") String token,
+            @RequestBody RequestEntity<UserInfo> requestEntity) {
+        return ResponseEntity.ok(Result.success(65536.0));
+    }
+
+    /**
+     * 测试HttpEntity
+     */
+    @RequestMapping("/user")
+    public HttpEntity<Result<UserInfo>> userInEntity(
+            @RequestHeader("token") String token) {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setAge(18);
+        return ResponseEntity.ok(Result.success(userInfo));
+    }
+
+}
