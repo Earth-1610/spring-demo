49ee2f27672d83dc77a7d93646313e2a3bdd8c1e
tangcent <sweettom@foxmail.com>
2019-11-28 21:32:22 +0800

    test rule: `method.additional.header`

---
+++ cn/common/src/main/java/com/itangcent/common/constant/UserTypeConstant.java
@@ -0,0 +1,16 @@
+package com.itangcent.common.constant;
+
+import java.io.Serializable;
+
+/**
+ * 用户类型
+ */
+public class UserTypeConstant implements Serializable {
+
+    private static final long serialVersionUID = -4607862808303533196L;
+
+    public static final int ADMIN = 1;//管理员
+    public static final int MEMBER = 2;//成员
+    public static final int GUEST = 3;//游客
+
+}
