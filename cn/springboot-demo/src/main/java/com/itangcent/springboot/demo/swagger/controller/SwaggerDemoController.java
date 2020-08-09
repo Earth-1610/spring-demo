e01c289d5c37647777484a7e0094c580b12d9e91
tangcent <pentatangcent@gmail.com>
2020-08-09 12:04:23 +0800

    chore: ApiImplicitParams&ApiResponses

--- cn/springboot-demo/src/main/java/com/itangcent/springboot/demo/swagger/controller/SwaggerDemoController.java
+++ cn/springboot-demo/src/main/java/com/itangcent/springboot/demo/swagger/controller/SwaggerDemoController.java
@@ -3,10 +3,7 @@ package com.itangcent.springboot.demo.swagger.controller;
 import com.itangcent.common.dto.Result;
 import com.itangcent.common.model.UserInfo;
 import com.itangcent.springboot.demo.swagger.dto.SwaggerModel;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.*;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,9 +31,20 @@ public class SwaggerDemoController {
     }
 
     @ApiOperation(value = "设置接口", notes = "这是设置接口", tags = {"swagger", "test"})
-    @RequestMapping(value = "/set", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/set/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ApiResponse(code = 300, message = "这是响应", response = UserInfo.class)
-    @ApiParam(value = "这是入参id", name = "这是method's name", defaultValue = "1")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "implicitQuery", value = "一个隐式的参数", defaultValue = "默认名字"),
+            @ApiImplicitParam(name = "implicitHeader", paramType = "header", value = "一个隐式的Header参数", defaultValue = "implicit_header"),
+            @ApiImplicitParam(name = "id", paramType = "path", value = "一个隐式的参数", defaultValue = "implicit_path"),
+            @ApiImplicitParam(name = "implicitForm", paramType = "form", value = "一个隐式的form参数", defaultValue = "implicit_form")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "正常返回"),
+            @ApiResponse(code = 401, message = "无权限"),
+            @ApiResponse(code = 403, message = "还是无权限")
+    })
+    @ApiImplicitParam(name = "implicit_query", value = "这是隐藏的参数", defaultValue = "implicit_query")
     public Object set(
             @ApiParam(value = "这是入参id", name = "这是param's name", defaultValue = "1")
                     long id,
